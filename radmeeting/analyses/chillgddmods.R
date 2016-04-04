d0c3c2b2eb4b3fad1d9628797ac884ecd3e6941d
AileneKane <aettinger@fasmail.harvard.edu>
2016-04-04 14:29:51 -0400

    Adding code for chilling models

---
+++ radmeeting/analyses/chillgddmods.R
@@ -0,0 +1,47 @@
+###Preliminary analyses of exp and obs data for radcliff
+##Started March 18, 2016
+library(RColorBrewer)
+library(lme4)
+library(car)
+##Read in climate and phenology data
+setwd("~/GitHub/radcliffe/radmeeting")
+expclim<-read.csv("expclim.csv", header=T)
+exppheno<-read.csv("exppheno.csv", header=T)
+obspheno<-read.csv("obspheno.csv", header=T)
+head(expclim)
+
+#Now, some preliminary analyses:
+#first, calculate gdd to expclim file:
+###Add GDD and cumulative gdd: soiltemp-tbase, cumulative GDD for that year (sum up to that date)
+expclim<-expclim[order(expclim$site,expclim$plot,expclim$year, expclim$doy),]
+
+tbase<-c(2,4,6,8,10)
+for (i in 1:length(tbase)){
+  expclim[,14+(i-1)+i]<-expclim$soiltemp1_mean-tbase[i]
+  expclim[,15+(i-1)+i]<-((as.numeric(expclim$airtemp_min)+as.numeric(expclim$airtemp_max))/2)-tbase[i]
+  expclim[,14+(i-1)+i][expclim[14+(i-1)+i] < tbase[i]] <- 0
+  expclim[,15+(i-1)+i][expclim[15+(i-1)+i] < tbase[i]] <- 0
+}
+for (i in 1:length(tbase)){
+  colnames(expclim)[14+(i-1)+i]<-paste("gdd_soil",tbase[i],sep=".")
+  colnames(expclim)[15+(i-1)+i]<-paste("gdd_air",tbase[i],sep=".")
+}
+#check
+#aggregate(expclim[, 15:24], list(expclim$site), mean, na.rm=T)
+#now add columns for cumulative
+cumsumnona <- function(x){cumsum(ifelse(is.na(x), 0, x)) + x*0}
+for (i in 1:length(tbase)){
+  expclim[,24+(i-1)+i]<-ave(expclim[,14+(i-1)+i],list(expclim$site,expclim$plot,expclim$year), FUN=cumsumnona)
+  expclim[,25+(i-1)+i]<-ave(expclim[,15+(i-1)+i],list(expclim$site,expclim$plot,expclim$year), FUN=cumsumnona)
+}
+for (i in 1:length(tbase)){
+  colnames(expclim)[24+(i-1)+i]<-paste("cumgdd_soil",tbase[i],sep=".")
+  colnames(expclim)[25+(i-1)+i]<-paste("cumgdd_air",tbase[i],sep=".")
+}
+expclim$alltreat<-paste(expclim$temptreat,expclim$preciptreat,sep=".")
+
+##Now calculate chilling days. First calculate all days that 
+tchill<-5
+expclim[which(expclim$soiltemp1_mean<tchill),]$chday<-1
+expclim[which(expclim$soiltemp1_mean>=tchill),]$chday<-0
+
--- test/testgit.R
+++
@@ -1,6 +0,0 @@
-###HEre we go trying out github!
-data<-c(1,2,3,4,)
-#Anne Marie
-data<-c(1,2,3,4,5)
-#isabelle
-data<-c(1,2,3,4,5,6)
