5c8745e598188548210eb632028f5ce1723f833f
AnneMariePanetta <anne.panetta@gmail.com>
2016-04-05 21:11:34 -0400

    Modifying code to assign a year-since-warming began to all sites

--- Analyses/teamnonstationarity/Duration_AMP.R
+++ Analyses/teamnonstationarity/Duration_AMP.R
@@ -9,8 +9,21 @@ expclim<-read.csv("Analyses/expclim.csv", header=TRUE)
 expinfo<-read.csv("Analyses/expsiteinfo.csv", header=TRUE)
 exptreat<-read.csv("Analyses/treats.csv", header=TRUE)
 
+###Adding a column to edata and eclim that has the start year of the experiment/observations
+#getting start years
+expinfo.start<-expinfo[,c("Site","exp_startyear")]
+expinfo.start<-setnames(expinfo.start, "Site", "site")
+
+#merging duration with data files
+expdata<-merge(expdata, expinfo.start, all=TRUE)
+expclim<-merge(expclim, expinfo.start, all=TRUE)
+
+#Creating a column with the number of years into the experiment (or the observational study) that data was collected
+expdata$year.num<-expdata$year-expdata$exp_startyear
+expclim$year.num<-expclim$year-expclim$exp_startyear
+
 #subsetting to just get Gothic data
-odata<-subset(odata, site %in% c("gothic")) #counts 162352 / 211952 (77% of all obs pheno data)
+odata<-subset(obsdata, site %in% c("gothic")) #counts 162352 / 211952 (77% of all obs pheno data)
 odata<-droplevels(odata)
 oclim<-subset(obsclim, site %in% c("gothic")) #49044/980880 (5% of all obs climate data)
 oclim<-droplevels(oclim)
@@ -20,25 +33,10 @@ edata<-droplevels(edata)
 eclim<-subset(expclim, site %in% c("price", "dunne")) #17979/327632 (5% of all experimental climate data)
 eclim<-droplevels(eclim)
 
-###Adding a column to edata and eclim that has the start year of the experiment
-#getting start years
-expinfo.start<-subset(expinfo, Site %in% c("price", "dunne"))
-expinfo.start<-expinfo.start[,c("Site","exp_startyear")]
-expinfo.start<-setnames(expinfo.start, "Site", "site")
-
-#merging duration with data files
-edata<-merge(edata, expinfo.start, all=TRUE)
-eclim<-merge(eclim, expinfo.start, all=TRUE)
-
-#adding start data column to obs
+#adding start data column to observational data (for gothic only)
 oclim$obs_startyear<-1973
 odata$obs_startyear<-1973
-
-#Creating a column with the number of years into the experiment (or the observational study) that data was collected
 oclim$year.num<-oclim$year-oclim$obs_startyear
-eclim$year.num<-eclim$year-eclim$exp_startyear
-edata$year.num<-edata$year-edata$exp_startyear
-eclim$year.num<-eclim$year-eclim$exp_startyear
-
+odata$year.num<-odata$year-odata$obs_startyear
 
 
