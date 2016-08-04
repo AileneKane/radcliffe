0ec7331f10719a821ba0ec8482c5384b428bb80f
AileneKane <aettinger@fasmail.harvard.edu>
2016-08-04 13:34:19 -0400

    direct and indirect effect figures

--- Analyses/wholegroup_paper1/Figures.R
+++ Analyses/wholegroup_paper1/Figures.R
@@ -71,3 +71,98 @@ mtext("(e.g. abundance of competing species)",side=1, line=2, adj=.5,cex=.8)
 y5<-rev(seq(261,280,by=2))
 plot(x,y2,type="l", lwd=2, xlab="", ylab="", bty="l", yaxt="n",xaxt="n")
 mtext("Air temperature",side=1, line=1, adj=.5)
+
+###Modifications to figure based on lizzie's feedback, plus new version of figure:
+quartz(width=11,height=4)
+par(mfrow=c(1,3),omi=c(.1,.2,.1,.8), mai=c(.6,.3,.5,1))
+x<-c(seq(1:10));y<-seq(261,270)
+plot(x,y,type="l", lty=2,lwd=1, xlab="", ylab="", bty="l", cex.lab=1.2,ylim=c(255,275), yaxt="n",xaxt="n")
+lines(x,y+.5,lty=1,lwd=2)#observed response
+mtext("a) Isolated treatment effect",side=3, line=2, adj=.5,cex=.9)
+mtext("Treatment intensity",side=1, line=1, adj=.5)
+mtext("(cooler", side=1, line=2, adj=0)
+mtext("warmer)", side=1, line=2, adj=1)
+text(7,272,"Observed",adj=0)
+text(7,271,"response",adj=0)
+text(7,266,"Effect of",adj=0)
+text(7,265,"temperature",adj=0)
+mtext("Response",side=2, line=1.5)
+mtext("(e.g. phenology, growth)",side=2, line=.5, cex=.9)
+
+#Panel b: muted response
+plot(x,y,type="l", lty=2,lwd=1, xlab="", ylab="", bty="l", cex.lab=1.2,ylim=c(255,275), yaxt="n",xaxt="n")
+y2<-rev(seq(256.5,261,by=.5))
+lines(x,y2,lty=3,lwd=1)#indirect effect
+y3<-seq(261,265.5,by=.5)
+lines(x,y3,lty=1,lwd=2)#observed response
+mtext("b) Muted observed response",side=3, line=2, adj=.5,cex=.9)
+mtext("Treatment intensity",side=1, line=1, adj=.5)
+mtext("(cooler", side=1, line=2, adj=0)
+mtext("warmer)", side=1, line=2, adj=1)
+text(7,266,"Effect of",adj=0)
+text(7,265,"temperature",adj=0)
+text(7,263.5,"Observed",adj=0)
+text(7,262.5,"response",adj=0)
+text(7,256.5,"Indirect",adj=0)
+text(7,255.5,"effect",adj=0)
+
+#Panel c: exaggerated response
+plot(x,y,type="l", lty=2,lwd=1, xlab="", ylab="", bty="l", cex.lab=1.2,ylim=c(255,275), yaxt="n",xaxt="n")
+y4<-seq(261,274.5,by=1.5)
+lines(x,y4,lty=3,lwd=1)#indirect effect
+y5<-seq(261,283.5,by=2.5)
+lines(x,y5,lty=1,lwd=2)#observed response
+mtext("c) Exaggerated observed response",side=3, line=2, adj=.5,cex=.9)
+mtext("Treatment intensity",side=1, line=1, adj=.5)
+mtext("(cooler", side=1, line=2, adj=0)
+mtext("warmer)", side=1, line=2, adj=1)
+text(7,266,"Effect of",adj=0)
+text(7,265,"temperature",adj=0)
+text(7,275,"Observed",adj=0)
+text(7,274,"response",adj=0)
+text(9,271.5,"Indirect",adj=0)
+text(9,270.5,"effect",adj=0)
+
+#Alternative bar graph version of figure
+quartz(width=11,height=4)
+par(mfrow=c(1,3),omi=c(.1,.2,.1,.8), mai=c(.6,.3,.5,1))
+x=barplot(c(1.5,NA,1.5),col=c("black",NA,"gray"),horiz=TRUE, space=c(1,1),xlim=c(-2,2),ylim=c(0,8), xlab="",ylab="")
+abline(v=0, lwd=1)
+mtext("Response",side=1, line=2)
+mtext("(e.g. phenology, growth)",side=1, line=3, cex=.9)
+text(1.5,x[1],"Effect of temperature", cex=1.5, adj=0)
+text(1.5,x[2],"Observed response", cex=1.5, adj=0)
+
+#Panel b: muted response
+plot(x,y,type="l", lty=2,lwd=1, xlab="", ylab="", bty="l", cex.lab=1.2,ylim=c(255,275), yaxt="n",xaxt="n")
+y2<-rev(seq(256.5,261,by=.5))
+lines(x,y2,lty=3,lwd=1)#indirect effect
+y3<-seq(261,265.5,by=.5)
+lines(x,y3,lty=1,lwd=2)#observed response
+mtext("b) Muted observed response",side=3, line=2, adj=.5,cex=.9)
+mtext("Treatment intensity",side=1, line=1, adj=.5)
+mtext("(cooler", side=1, line=2, adj=0)
+mtext("warmer)", side=1, line=2, adj=1)
+text(7,266,"Effect of",adj=0)
+text(7,265,"temperature",adj=0)
+text(7,263.5,"Observed",adj=0)
+text(7,262.5,"response",adj=0)
+text(7,256.5,"Indirect",adj=0)
+text(7,255.5,"effect",adj=0)
+
+#Panel c: exaggerated response
+plot(x,y,type="l", lty=2,lwd=1, xlab="", ylab="", bty="l", cex.lab=1.2,ylim=c(255,275), yaxt="n",xaxt="n")
+y4<-seq(261,274.5,by=1.5)
+lines(x,y4,lty=3,lwd=1)#indirect effect
+y5<-seq(261,283.5,by=2.5)
+lines(x,y5,lty=1,lwd=2)#observed response
+mtext("c) Exaggerated observed response",side=3, line=2, adj=.5,cex=.9)
+mtext("Treatment intensity",side=1, line=1, adj=.5)
+mtext("(cooler", side=1, line=2, adj=0)
+mtext("warmer)", side=1, line=2, adj=1)
+text(7,266,"Effect of",adj=0)
+text(7,265,"temperature",adj=0)
+text(7,275,"Observed",adj=0)
+text(7,274,"response",adj=0)
+text(9,271.5,"Indirect",adj=0)
+text(9,270.5,"effect",adj=0)
