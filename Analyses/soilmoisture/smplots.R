4a481433d637ee1829bf890af2209bc1508bacee
AileneKane <aettinger@fasmail.harvard.edu>
2020-08-22 22:31:25 -0700

    sm vs phenology by site

---
+++ Analyses/soilmoisture/smplots.R
@@ -0,0 +1,54 @@
+# Plotting BB vs soil moisture, bt site
+#Does there appear to be a threshold in soil moisture effect on budburst
+
+## housekeeping
+rm(list=ls()) 
+options(stringsAsFactors = FALSE)
+
+# dostan = TRUE
+library(lme4)
+library(rstan)
+library(ggplot2)
+library(shinystan)
+library(bayesplot)
+library(rstanarm)
+library(dplyr)
+library(brms)
+library(RColorBrewer)
+library(plotrix)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+#options(mc.cores = parallel::detectCores()) # added by Andrew
+#update.packages()
+
+# Setting working directory. Add in your own path in an if statement for your file structure
+if(length(grep("ailene", getwd()))>0) {setwd("/Users/aileneettinger/Documents/GitHub/radcliffe")}
+#setwd("~/Documents/GitHub/radcliffe")#noaa
+#Goal: Fit a multi-model to phenology (budburst) data with temperature, soil moisture, and 
+#their interaction as explanatory variables.
+#
+###Now with the data
+
+#source('Analyses/soilmoisture/savestan.R')
+
+#Read in experimental climate and phenology data
+expclim<-read.csv("Analyses/gddchill/expclim.wchillgdd.csv", header=TRUE)
+exppheno<-read.csv("Analyses/exppheno.csv", header=TRUE)
+treats<-read.csv("Analyses/treats_detail.csv", header=T)
+
+#Make some choices about how to restrict the data:
+remove.conifers=TRUE
+use.airtemp=TRUE
+#standard data wrangling to get expclim2 for climate analyses and expgdd for phenology analyses (with gddcrit)
+source("Analyses/source/standard_mergesandwrangling.R")
+
+#Summarize climate data by plot (annual and seasonal temp, soil mois), 
+#merge in with expgdd file, and select out only sites with soil moisture and air temperature data, and remove NAs
+#source("Analyses/soilmoisture/climsum_byplot_soiltoo.R")#doesn't work for some reason....
+source("Analyses/source/climsum_byplot.R")
+
+#Prepare data for phenology models in stan#####
+source("Analyses/source/stanprep_phenmods.R")
+
+######
