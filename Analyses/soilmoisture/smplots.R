e735d292eed8602b0cbac9062a387d5aa5b77a0d
AileneKane <aettinger@fasmail.harvard.edu>
2020-09-24 12:52:04 -0700

    updates to soil moisture

--- Analyses/soilmoisture/smplots.R
+++ Analyses/soilmoisture/smplots.R
@@ -51,4 +51,36 @@ source("Analyses/source/climsum_byplot.R")
 #Prepare data for phenology models in stan#####
 source("Analyses/source/stanprep_phenmods.R")
 
-######
+######start with budburst
+sites<-unique(expgdd_bbd$site2)
+pdf(file ="Analyses/figures/sitesm.bbd.pdf", width = 8, height = 6)
+  par(mfrow= c(2,3))
+    for(i in 1:length(sites)){
+  sitedat<-expgdd_bbd[expgdd_bbd$site2 == sites[i],]
+      plot(sitedat$soilmois_janmar,sitedat$doy, main = paste(sites[i]," bbd", sep = ""), 
+           ylim= c(min(expgdd_bbd$doy),max(expgdd_bbd$doy)),
+           xlim=c(min(expgdd_bbd$soilmois_janmar), max(expgdd_bbd$soilmois_janmar)))
+}
+dev.off()
+
+sites<-unique(expgdd_lod$site2)
+pdf(file ="Analyses/figures/sitesm.lod.pdf", width = 8, height = 6)
+par(mfrow= c(2,3))
+for(i in 1:length(sites)){
+  sitedat<-expgdd_lod[expgdd_lod$site2 == sites[i],]
+  plot(sitedat$soilmois_janmar,sitedat$doy, main = paste(sites[i]," lod", sep = ""), 
+       ylim= c(min(expgdd_lod$doy),max(expgdd_lod$doy)),
+       xlim=c(min(expgdd_lod$soilmois_janmar), max(expgdd_lod$soilmois_janmar)))
+}
+dev.off()
+
+sites<-unique(expgdd_ffd$site2)
+pdf(file ="Analyses/figures/sitesm.ffd.pdf", width = 8, height = 6)
+par(mfrow= c(2,3))
+for(i in 1:length(sites)){
+  sitedat<-expgdd_ffd[expgdd_ffd$site2 == sites[i],]
+  plot(sitedat$soilmois_janmar,sitedat$doy, main = paste(sites[i]," ffd", sep = ""), 
+       ylim= c(min(expgdd_ffd$doy),max(expgdd_ffd$doy)),
+       xlim=c(min(expgdd_ffd$soilmois_janmar), max(expgdd_ffd$soilmois_janmar)))
+}
+dev.off()
